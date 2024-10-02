98909d69c12ebcba92b14bec7020c664bdc52d16
Prajanya-Sharma <prajanyasharma.bt23ele@pec.edu.in>
2024-10-02 17:57:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Board from "./Tic-Tac-Toe/Board";
+import Title from "./Tic-Tac-Toe/Title";
+
+
+function App(){
+  return ( <div className="App">
+    <Title/>
+<Board/>
+  </div>
+  )
+}
+
+export default App;
---
+++ Tic-Tac-Toe/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Title =()=>{
+    return (
+        <div className="Title">
+            <h1>Tic Tac Toe</h1>
+            <h3>X makes the first move</h3>
+            <h5>Enjoy your game!!!</h5>
+        </div>
+    )
+}
+
+export default Title;
